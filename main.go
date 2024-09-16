0a4a7594160b36924acc58b383e7ed4f0fd077d9
Ankit Nayan <ankit@signoz.io>
2024-09-16 18:09:05 +0530

    sending flat spans to hc

--- main.go
+++ main.go
@@ -2,97 +2,63 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
-	"os"
-	"strings"
+	"net/http"
+	"time"
 
-	"github.com/SigNoz/sample-golang-app/controllers"
-	"github.com/SigNoz/sample-golang-app/metrics"
-	"github.com/SigNoz/sample-golang-app/models"
-	"google.golang.org/grpc/credentials"
-
-	"github.com/gin-gonic/gin"
-	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"github.com/honeycombio/otel-config-go/otelconfig"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-
-	"go.opentelemetry.io/otel/sdk/resource"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
 )
 
-var (
-	serviceName  = os.Getenv("SERVICE_NAME")
-	collectorURL = os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
-	insecure     = os.Getenv("INSECURE_MODE")
-)
-
-func initTracer() func(context.Context) error {
-
-	var secureOption otlptracegrpc.Option
-
-	if strings.ToLower(insecure) == "false" || insecure == "0" || strings.ToLower(insecure) == "f" {
-		secureOption = otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
-	} else {
-		secureOption = otlptracegrpc.WithInsecure()
+func createFlatSpans(ctx context.Context, tracer trace.Tracer, remaining int, current int) {
+	if remaining == 0 {
+		return
 	}
+	// time.Sleep(10 * time.Millisecond)
+	_, span := tracer.Start(ctx, fmt.Sprintf("flat-span-%d", current))
+	defer span.End()
 
-	exporter, err := otlptrace.New(
-		context.Background(),
-		otlptracegrpc.NewClient(
-			secureOption,
-			otlptracegrpc.WithEndpoint(collectorURL),
-		),
-	)
+	span.SetAttributes(attribute.Int("flat-number", current))
 
-	if err != nil {
-		log.Fatalf("Failed to create exporter: %v", err)
-	}
-	resources, err := resource.New(
-		context.Background(),
-		resource.WithAttributes(
-			attribute.String("service.name", serviceName),
-			attribute.String("library.language", "go"),
-		),
-	)
-	if err != nil {
-		log.Fatalf("Could not set resources: %v", err)
-	}
-
-	otel.SetTracerProvider(
-		sdktrace.NewTracerProvider(
-			sdktrace.WithSampler(sdktrace.AlwaysSample()),
-			sdktrace.WithBatcher(exporter),
-			sdktrace.WithResource(resources),
-		),
-	)
-	return exporter.Shutdown
+	// Recursively create the next nested span using the current span's context
+	createFlatSpans(ctx, tracer, remaining-1, current+1)
 }
 
-func main2() {
+// Implement an HTTP Handler func to be instrumented
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	tracer := otel.Tracer("github.com/SigNoz/sample-golang-app/controllers")
 
-	cleanup := initTracer()
-	defer cleanup(context.Background())
+	// Start with the root span
+	ctx, rootSpan := tracer.Start(ctx, "FindBooks-root")
+	defer rootSpan.End()
 
-	provider := metrics.InitMeter()
-	defer provider.Shutdown(context.Background())
+	// Create 20000 nested spans
+	createFlatSpans(ctx, tracer, 20000, 0)
+	time.Sleep(10 * time.Second)
+	fmt.Fprintf(w, "Hello, World")
+}
 
-	meter := provider.Meter("sample-golang-app")
-	metrics.GenerateMetrics(meter)
+// Wrap the HTTP handler func with OTel HTTP instrumentation
+func wrapHandler() {
+	handler := http.HandlerFunc(httpHandler)
+	wrappedHandler := otelhttp.NewHandler(handler, "hello")
+	http.Handle("/hello", wrappedHandler)
+}
 
-	r := gin.Default()
-	r.Use(otelgin.Middleware(serviceName))
-	// Connect to database
-	models.ConnectDatabase()
+func main() {
+	// use otelconfig to setup OpenTelemetry SDK
+	otelShutdown, err := otelconfig.ConfigureOpenTelemetry()
+	if err != nil {
+		log.Fatalf("error setting up OTel SDK - %e", err)
+	}
+	defer otelShutdown()
 
-	// Routes
-	r.GET("/books", controllers.FindBooks)
-	r.GET("/books/:id", controllers.FindBook)
-	r.POST("/books", controllers.CreateBook)
-	r.PATCH("/books/:id", controllers.UpdateBook)
-	r.DELETE("/books/:id", controllers.DeleteBook)
-
-	// Run the server
-	r.Run(":8090")
+	// Initialize HTTP handler instrumentation
+	wrapHandler()
+	log.Fatal(http.ListenAndServe(":3030", nil))
 }
